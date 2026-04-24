644d6643d4bcca6c1e83d355f698d5267c575a16
evanshown <evan@shownmedia.com>
2026-04-24 11:17:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/viral-launchpad-fake-shownie">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
